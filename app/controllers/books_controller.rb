bcbc69b0f0c5c3a28a6ed4c6dc1d359408d2ffb4
Keli Booster <kbooster17@gmail.com>
2020-10-23 00:32:03 +0000

    add create method in books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,20 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create 
+    book = Book.new(books_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unproccessable_entity
+    end
+  end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:title, :author, :descriiption )
+  end
+
 end
