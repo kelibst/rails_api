19ff1d49fcd740fa42ae51eba34fe2935a1dc6bd
Keli Booster <kbooster17@gmail.com>
2020-10-27 14:30:57 +0000

    add registration and sessions controllers and jbuilders

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :first_name, presence: true, allow_blank: false
+  validates :last_name, presence: true, allow_blank: false
+
+  def generate_jwt
+    JWT.encode({ id: id,
+              exp: 30.days.from_now.to_i },
+              Rails.application.secrets.secret_key_base)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace api do
-    namespace v1 do
+  namespace :api do
+    namespace :v1,defaults: { format: :json } do
         devise_for :users
     end
   end
