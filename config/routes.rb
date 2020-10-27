5d437464f5aa1336262d04ffe92a0c2994d5c4c7
Keli Booster <kbooster17@gmail.com>
2020-10-27 14:12:45 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace api do
+    namespace v1 do
+        devise_for :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
