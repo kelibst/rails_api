ab6d601ec85bf0981d7dc959ab72e453df4e13aa
Keli Booster <kbooster17@gmail.com>
2020-10-28 10:00:41 +0000

    update application.rb

---
+++ app/controllers/api/v1/users/users_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+  class UsersController < ApplicationController
+      before_action :authenticate_user!
+    
+      def show
+      end
+
+      def update
+      if current_user.update_attributes(user_params)
+          render :show
+      else
+          render json: { errors: current_user.errors }, status: :unprocessable_entity
+      end
+      end
+      
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :bio, :image)
+    end
+  end
+  end
+end
